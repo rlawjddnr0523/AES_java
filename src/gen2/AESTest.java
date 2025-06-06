2f6ef5b670e394dc61881af5bba9923a418a4761
김정욱 <yeoback07@icloud.com>
2025-06-06 21:03:32 +0900

    (feat: /gen3/$AES) generation 3 feature
    AES, Functions, Table
    AES-128, 192, 256 support
    ECB, CBC mode support
    No padding, Pkcs7 Padding support
    CLI Commands Supported
    All codes have descriptions on their head. (Korean)

--- src/gen2/AESTest.java
+++ src/gen2/AESTest.java
@@ -49,7 +49,7 @@ public class AESTest {
                 {(byte)0x00,(byte)0x11,(byte)0x22,(byte)0x33,(byte)0x44,(byte)0x55,(byte)0x66,(byte)0x77,(byte)0x88,(byte)0x99,(byte)0xaa,(byte)0xbb,(byte)0xcc,(byte)0xdd,(byte)0xee,(byte)0xff},
         };
         
-        byte[][] RK = Functions.keySchedule(K, false);
+        byte[][] RK = Functions.keySchedule(K, true);
         byte[][] res = Functions.addRoundKey(P, RK, 0, false);
         System.out.println("======0======");
         System.out.println("Expected: "+gen1.AES.bytesToHex(ee[0]));
